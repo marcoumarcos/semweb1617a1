c1ea26f304a33cdea141b90dedad3ef641e861ee
marcoumarcos <m.marcou14@gmail.com>
2017-06-01 16:08:07 +0300

    Update bacon.py

--- bacon.py
+++ bacon.py
@@ -1,24 +1,96 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 
 from urllib.request import urlopen,Request
 from urllib.parse import urlencode
+import json
 
-#import json	# κρατήστε αυτό αν χειρίζεστε αποτελέσματα σε μορφή json...
-#import xml.etree.ElementTree as ET	# ...ή αυτό για αποτελέσματα σε xml
+endpoint = "http://data.linkedmdb.org/sparql?"
 
+lista = []
+ithopoios = input ('dwste to mikro onoma tou ithopoiou: ')
 
-# ζητήστε ένα μικρό όνομα από τον χρήστη
+sparqlq = """
+ PREFIX movie: <http://data.linkedmdb.org/resource/movie/>
+ PREFIX lmdba: <http://data.linkedmdb.org/resource/actor/>
+ PREFIX lmdbf: <http://data.linkedmdb.org/resource/film/>
+ PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 
+ SELECT ?actor ?aname WHERE {
+  ?actor a movie:actor .
+  ?actor movie:actor_name ?aname.
+  FILTER (regex(?aname,"^""" + ithopoios + """","i"))
+  	
+}
+"""
 
-# με την πρώτη ερώτηση SPARQL ανακτήστε λίστα ηθοποιών (και τα URIs τους) με το όνομα αυτό
+# params sent to server
+params = { 'query': sparqlq }
+# create appropriate param string
+paramstr = urlencode(params)
 
+# create GET http request object with params appended
+req = Request(endpoint+paramstr)
+# request specific content type
+req.add_header('Accept','application/sparql-results+json')
+# dispatch request
+page = urlopen(req)
+# get results and close
+text = page.read().decode('utf-8')
+page.close()
 
-# ζητήστε από τον χρήστη να επιλέξει ηθοποιό
+#print text
 
+# convert to json object
+jso = json.loads(text)
 
-# με τη δεύτερη ερώτηση SPARQL βρείτε τους συμπρωταγωνιστές/ταινίες
+# iterate over results
+for binding in jso['results']['bindings']:
+	# for every column in binding
+   lista.append ([binding['actor']['value'], binding['aname']['value']])
 
+for i, item in enumerate(lista):
+	print (i+1, item[1])
 
-# τυπώστε ονόματα και τίτλους ταινιών
+arithmos = int (input ('epilexte ton arithmo tou ithopoiou: ')) -1
 
+sparqlq = """
+PREFIX movie: <http://data.linkedmdb.org/resource/movie/>
+PREFIX lmdba: <http://data.linkedmdb.org/resource/actor/>
+PREFIX lmdbf: <http://data.linkedmdb.org/resource/film/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
+SELECT ?actor ?film ?actorname ?moviename WHERE{
+?film movie:actor <"""+lista[arithmos][0]+"""> .
+?film movie:actor ?actor . 
+?film rdfs:label ?moviename .
+?actor movie:actor_name ?actorname .
+FILTER (?actor != <"""+lista[arithmos][0]+"""> )
+}
+"""
 
+# params sent to server
+params = { 'query': sparqlq }
+# create appropriate param string
+paramstr = urlencode(params)
+
+# create GET http request object with params appended
+req = Request(endpoint+paramstr)
+# request specific content type
+req.add_header('Accept','application/sparql-results+json')
+# dispatch request
+page = urlopen(req)
+# get results and close
+text = page.read().decode('utf-8')
+page.close()
+
+#print text
+
+# convert to json object
+jso = json.loads(text)
+
+# iterate over results
+for binding in jso['results']['bindings']:
+	# for every column in binding
+	print  (binding['actorname']['value'], binding['moviename']['value'])
